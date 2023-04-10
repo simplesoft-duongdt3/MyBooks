401676bf7a9d19c1e19e09f560b20610d7259065
duongdt <doanthanhduong11031990@gmail.com>
2023-04-10 22:07:06 +0700

    add api /draft-books + api /books

--- api.py
+++ api.py
@@ -12,14 +12,21 @@ import codecs
 from img2vec_pytorch import Img2Vec
 from PIL import Image
 from sklearn.metrics.pairwise import cosine_similarity
-import logging
+from response_models import BookListResponse, DraftBookListResponse
+
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger('app_log')
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 api_endpoint = "http://103.221.220.249:8080"
 # Initialize Img2Vec with GPU
 img2vec = Img2Vec(cuda=False, model='resnet18')
+
 
 
 def check_feature_vector_all_exists_books(image_feature_vector):
@@ -58,10 +65,44 @@ def check_feature_vector_all_exists_books(image_feature_vector):
                         'thumb_image': thumb_image,
                         'distance': str(distance)
                     })
-                print("Id = " + str(book_id) + " distance = " + str(distance))
+                logger.info("Id = " + str(book_id) + " distance = " + str(distance))
     time_end = time.time()
-    print("check_feature_vector_all_exists_books time= " + str(time_end - time_start))
+    logger.info("check_feature_vector_all_exists_books time= " + str(time_end - time_start))
     return list_book_similar
+
+def get_draft_book_list_paging(page_index: int = 1, page_size: int = 20) -> DraftBookListResponse:    
+    time_start = time.time()
+    url = f"{api_endpoint}/api/v1/db/data/v1/MyBooks/DraftBooks?fields=Id%2CThumbImage%2CCreatedAt%2CUpdatedAt%2CStatus&sort=-UpdatedAt&where=where%3D%28Status%2Ceq%2CActive%29&limit={page_size}&offset={page_index - 1}"
+
+    payload={}
+    headers = {
+        'accept': 'application/json',
+        'xc-token': '5Bdhl77iJJ1fIbF2fPb8hcCceNzSJvmt4NIya0aR'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    list_draft_book : DraftBookListResponse = DraftBookListResponse.parse_raw(response.text)
+    
+    time_end = time.time()
+    logger.info("get_draft_book_list_paging time= " + str(time_end - time_start))
+    return list_draft_book
+
+def get_book_list_paging(page_index: int = 1, page_size: int = 20) -> DraftBookListResponse:    
+    time_start = time.time()
+    url = f"{api_endpoint}/api/v1/db/data/v1/MyBooks/Books?fields=Id%2CName%2CAuthors%2CPublished%20Year%2CPublished%20By%2CStatus%2CThumbImage%2CBookCollections%20List%2CCreatedAt%2CUpdatedAt&sort=-UpdatedAt&where=where%3D%28Status%2Ceq%2CActive%29&limit={page_size}&offset={page_index-1}"
+
+    payload={}
+    headers = {
+        'accept': 'application/json',
+        'xc-token': '5Bdhl77iJJ1fIbF2fPb8hcCceNzSJvmt4NIya0aR'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    list_book : BookListResponse = BookListResponse.parse_raw(response.text)
+    
+    time_end = time.time()
+    logger.info("get_book_list_paging time= " + str(time_end - time_start))
+    return list_book
 
 def get_image_feature_vector(file_content):
     # Read in an image (rgb format)
@@ -78,30 +119,30 @@ async def root():
     img = Image.open('b01.jpg')
     # Get a vector from img2vec, returned as a torch FloatTensor
     vec = img2vec.get_vec(img, tensor=True)
-    #print(vec)
+    #logger.info(vec)
     pickled = pickle.dumps(vec)
 
-    print(f"length of {len(pickled)}")
+    logger.info(f"length of {len(pickled)}")
 
     base64Data = codecs.encode(pickled, "base64").decode()
 
-    print(base64Data)
+    logger.info(base64Data)
 
     decodeData = pickle.loads(codecs.decode(base64Data.encode(),'base64'))
 
-    print(f"length of {len(decodeData)}")
-    #print(decodeData)
+    logger.info(f"length of {len(decodeData)}")
+    #logger.info(decodeData)
 
     distance = cosine_similarity(vec.reshape((1, -1)), decodeData.reshape((1, -1)))[0][0]
     time_end = time.time()
-    print("distance = " + str(distance) + " time= " + str(time_end - time_start))
+    logger.info("distance = " + str(distance) + " time= " + str(time_end - time_start))
 
     img2 = Image.open('b02.jpg')
     vec2 = img2vec.get_vec(img2, tensor=True)
 
     distance2 = cosine_similarity(vec.reshape((1, -1)), vec2.reshape((1, -1)))[0][0]
     time_end = time.time()
-    print("distance2 = " + str(distance2) + " time= " + str(time_end - time_start))
+    logger.info("distance2 = " + str(distance2) + " time= " + str(time_end - time_start))
     
 
     img3 = Image.open('b01_1.jpg')
@@ -109,12 +150,12 @@ async def root():
 
     distance3 = cosine_similarity(vec.reshape((1, -1)), vec3.reshape((1, -1)))[0][0]
     time_end = time.time()
-    print("distance3 = " + str(distance3) + " time= " + str(time_end - time_start))
+    logger.info("distance3 = " + str(distance3) + " time= " + str(time_end - time_start))
     return {"message": "Hello World"}
 
 def create_draft_book(result_upload, feature_vector_base64):
     try:
-        print(f'create_draft_book {result_upload}')
+        logger.info(f'create_draft_book {result_upload}')
         url = f"{api_endpoint}/api/v1/db/data/v1/MyBooks/DraftBooks"
 
         payload = json.dumps({
@@ -130,13 +171,13 @@ def create_draft_book(result_upload, feature_vector_base64):
         response = requests.request("POST", url, headers=headers, data=payload)
         return json.loads(response.text)
     except Exception:
-        logging.exception("create_draft_book: An exception was thrown!")
+        logger.exception("create_draft_book: An exception was thrown!")
         return None
 
 def create_book(thumbImage, feature_vector_base64, name, author, published_year, published_by):
     try:
         time_start = time.time()
-        logging.info(f'create_book {str(name)} {str(thumbImage)}')
+        logger.info(f'create_book {str(name)} {str(thumbImage)}')
         url = f"{api_endpoint}/api/v1/db/data/v1/MyBooks/Books"
 
         payload = json.dumps({
@@ -154,12 +195,12 @@ def create_book(thumbImage, feature_vector_base64, name, author, published_year,
         }
 
         response = requests.request("POST", url, headers=headers, data=payload)
-        logging.info(f'create_book response {str(response)}')
+        logger.info(f'create_book response {str(response)}')
         time_end = time.time()
-        logging.info("create_book time= " + str(time_end - time_start))
+        logger.info("create_book time= " + str(time_end - time_start))
         return json.loads(response.text)
     except Exception:
-        logging.exception("create_book: An exception was thrown!")
+        logger.exception("create_book: An exception was thrown!")
         return None
 
 def encode_base64_feature_vector(feature_vector):
@@ -170,7 +211,7 @@ def encode_base64_feature_vector(feature_vector):
 def upload_file_image(file_name, file_contents, content_type):
     try:
         time_start = time.time()
-        logging.info(f"upload_file_image: file_name {file_name} content_type {content_type}")
+        logger.info(f"upload_file_image: file_name {file_name} content_type {content_type}")
         today = datetime.now()
         str_date_time = today.strftime("%Y%m%d")
 
@@ -187,13 +228,13 @@ def upload_file_image(file_name, file_contents, content_type):
         response = requests.request("POST", url, headers=headers, data=payload, files=files)
 
         response_list = json.loads(response.text)
-        logging.info(f"upload_file_image response {str(response_list)}")
+        logger.info(f"upload_file_image response {str(response_list)}")
 
         time_end = time.time()
-        logging.info("upload_draft_image time= " + str(time_end - time_start))
+        logger.info("upload_file_image time= " + str(time_end - time_start))
         return response_list
     except Exception as e:
-        logging.exception("upload_file_image: An exception was thrown!")
+        logger.exception("upload_file_image: An exception was thrown!")
         return None
 
 @app.post("/upload")
@@ -216,17 +257,48 @@ async def upload_draft_image(
             result_create_draft_book = create_draft_book(response_list, feature_vector_base64)
 
         time_end = time.time()
-        logging.info("upload_draft_image time= " + str(time_end - time_start))    
+        logger.info("upload_draft_image time= " + str(time_end - time_start))    
         return {
-            # "file_name": file.filename,
-            # "file_size": file.size,
-            # "file": base64_message,
-            # "file_content_type": file.content_type,
-            "result_create_draft_book": result_create_draft_book,
+            "draft_book": result_create_draft_book,
             'list_book_similar': list_book_similar
         }
     except Exception as e:
-        logging.exception("upload_draft_image: An exception was thrown!")
+        logger.exception("upload_draft_image: An exception was thrown!")
         raise HTTPException(status_code=500, detail="Error: " + str(e))
     finally:
         file.file.close()
+
+@app.get("/draft-books")
+async def get_draft_books(page: int = 1, limit: int = 20) -> DraftBookListResponse:
+    try:
+        time_start = time.time()
+        list_result: DraftBookListResponse = get_draft_book_list_paging(
+            page_index=page - 1,
+            page_size=limit,
+        )
+        time_end = time.time()
+        logger.info("get_draft_books time= " + str(time_end - time_start))
+        return list_result
+    except Exception as e:
+        logger.exception("get_draft_books: An exception was thrown!")
+        return None
+    
+@app.get("/books")
+async def get_books(page: int = 1, limit: int = 20) -> BookListResponse:
+    try:
+        time_start = time.time()
+        list_result: BookListResponse = get_book_list_paging(
+            page_index=page - 1,
+            page_size=limit,
+        )
+        time_end = time.time()
+        logger.info("get_books time= " + str(time_end - time_start))
+        return list_result
+    except Exception as e:
+        logger.exception("get_books: An exception was thrown!")
+        return None
+    
+    # 1. Add Model response for API create_draft_book
+    # 2. API get draft book detail = respone of API create_draft_book
+    # 3. API create book from draft book
+    
--- response_models.py
+++ response_models.py
@@ -4,16 +4,17 @@ class ThumbImageItem(BaseModel):
     mimetype: str
     size: int
     title: str
-    url: str
+    path: str
 
 class DraftBook(BaseModel):
     Id: int
     ThumbImage: list[ThumbImageItem] | None = None
     CreatedAt: str
     UpdatedAt: str
+    Status: str
 
 class DraftBookListResponse(BaseModel):
-    list: list[DraftBook] | None = None
+    list: list[DraftBook] | None
 
 class BookCollectionsList(BaseModel):
     Id: int
@@ -28,13 +29,13 @@ class Book(BaseModel):
     CreatedAt: str
     UpdatedAt: str
     Authors: str | None = None
-    Published_Year: str | None = Field(..., alias='Published Year', default=None)
-    Published_By: str | None = Field(..., alias='Published By', default=None)
+    Published_Year: str | None = Field(alias='Published Year', default=None)
+    Published_By: str | None = Field(alias='Published By', default=None)
     Status: str
     ThumbImage: list[ThumbImageItem] | None = None
-    BookCollections_List: BookCollectionsList | None = Field(..., alias='BookCollections List', default=None)
+    BookCollections_List: list[BookCollectionsList] | None = Field(alias='BookCollections List', default=None)
 
 
 class BookListResponse(BaseModel):
-    list: list[Book] | None = None
+    list: list[Book] | None
     
