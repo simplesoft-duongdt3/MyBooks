79af2098fa8b1d52bad21cc69950460d23f1dd4d
duongdt <doanthanhduong11031990@gmail.com>
2023-04-22 22:54:47 +0700

    add test redis images

--- Dockerfile
+++ Dockerfile
@@ -10,3 +10,5 @@ COPY ./app /app
 
 # docker build -t my_books_image:latest .
 # docker run -d --name my_books -p 8000:8000 my_books_image:latest
+
+# docker run -d --name my_books_redis -v ${PWD}/local_data_redis:/data -p 10001:6379 -p 13333:8001 redis/redis-stack:latest
--- app/test_redis_vector.py
+++ app/test_redis_vector.py
@@ -1,6 +1,8 @@
+import codecs
+import base64
 import random
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import time
 from redis import Redis
 from redis.commands.search.field import VectorField
@@ -14,8 +16,12 @@ from img2vec_pytorch import Img2Vec
 img2vec = Img2Vec(cuda=False, model='resnet18')
 
 host = 'localhost'
-port = 6379
-redis_conn = Redis(host = host, port = port)
+port = 10001
+redis_conn = Redis(
+    host = host, port = port, 
+    encoding="utf-8",
+    decode_responses=False,
+    )
 
 def create_flat_index (redis_conn,vector_field_name,number_of_vectors, vector_dimensions=512, distance_metric='L2'):
     redis_conn.ft().create_index([
@@ -36,6 +42,14 @@ NUMBER_PRODUCTS = 1000
 IMAGE_VECTOR_DIMENSION = 512
 DISTANCE_METRIC = 'COSINE'
 
+def insert_redis(redis_conn, id, vector_bytes):
+    item_metadata = {
+        'id' : f'{id}',
+        'name' : f'Image {id}',
+        PRODUCT_IMAGE_VECTOR_FIELD: vector_bytes,
+    }
+    redis_conn.hset(f'{id}',mapping=item_metadata)
+
 if __name__ == "__main__":
     print ('Connecting to redis')
     redis_conn.ping()
@@ -43,7 +57,6 @@ if __name__ == "__main__":
     redis_conn.flushall()
 
     create_flat_index(redis_conn, PRODUCT_IMAGE_VECTOR_FIELD,NUMBER_PRODUCTS,IMAGE_VECTOR_DIMENSION,DISTANCE_METRIC)
-
 
     vector = get_image_feature_vector('test01.jpg')
     # print(type(vector))
@@ -58,25 +71,49 @@ if __name__ == "__main__":
     # print(vector_float32)
 
     vector_bytes = vector_float32.tobytes()
-    # print(type(vector_bytes))
-    # print(len(vector_bytes))
-    # print(vector_bytes)
-    item_metadata = {
-        'id' : '001',
-        'name' : 'Image 001',
-        PRODUCT_IMAGE_VECTOR_FIELD: vector_bytes,
-    }
+    # Encode 1
+    base64_bytes = base64.b64encode(vector_bytes)
+    base64_message = base64_bytes.decode()
+
+    print(f'base64_message 1 {base64_message}')
+
+    # Decode
+    base64_bytes = base64_message.encode()
+    message_bytes = base64.b64decode(base64_bytes)
+
+    # Encode 2
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode()
+
+    print(f'base64_message 2 {base64_message}')
+
+
+    vector_verify = get_image_feature_vector('test01_verify.jpg')
+    vector_verify_float32 = vector_verify.astype(np.float32)
+    vector_verify_bytes = vector_verify_float32.tobytes()
+
+    vector_verify2 = get_image_feature_vector('test01_verify2.jpg')
+    vector_verify2_float32 = vector_verify2.astype(np.float32)
+    vector_verify2_bytes = vector_verify2_float32.tobytes()
+
+    # item_metadata = {
+    #     'id' : '001',
+    #     'name' : 'Image 001',
+    #     PRODUCT_IMAGE_VECTOR_FIELD: vector_bytes,
+    # }
+    # redis_conn.hset('001',mapping=item_metadata)
+    insert_redis(redis_conn, '001', vector_bytes)
+    insert_redis(redis_conn, '002', vector_verify_bytes)
+    insert_redis(redis_conn, '003', vector_verify2_bytes)
     # For multi records
     # p = redis_conn.pipeline(transaction=False)
     # p.hset('001',mapping=item_metadata)
 
-    redis_conn.hset('001',mapping=item_metadata)
-    
 
     print(f'Keys redis size {len(redis_conn.keys())}')
 
     topK=5
-    query_vector = vector_bytes
+    query_vector = vector_verify2_bytes
 
     #prepare the query
     q = Query(f'*=>[KNN {topK} @{PRODUCT_IMAGE_VECTOR_FIELD} $vec_param AS vector_score]').sort_by('vector_score').paging(0,topK).return_fields('vector_score','id','name').dialect(2)
