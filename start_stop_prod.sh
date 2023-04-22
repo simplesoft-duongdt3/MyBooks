50209599bfde47baa17d8cc58aba807a140a382e
Doan Thanh Duong <doanthanhduong11031990@gmail.com>
2023-04-22 14:26:43 +0700

    add install requirements script

--- start_stop_prod.sh
+++ start_stop_prod.sh
@@ -1,4 +1,7 @@
+# python3.10 -m virtualenv venv
 source venv/bin/activate
+
+# python -m pip install -r requirements.txt
 
 cd app
 gunicorn api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000  --daemon
