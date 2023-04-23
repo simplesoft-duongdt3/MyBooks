ca4bb9e788763bc950662ef0a1556e994d4c0332
duongdt <doanthanhduong11031990@gmail.com>
2023-04-23 08:16:49 +0700

    update folder test ai

--- test_ai/test_redis_vector.py
+++ test_ai/test_redis_vector.py
@@ -11,9 +11,44 @@ from redis.commands.search.field import TagField
 from redis.commands.search.query import Query
 from PIL import Image
 from img2vec_pytorch import Img2Vec
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 # Initialize Img2Vec without GPU
-img2vec = Img2Vec(cuda=False, model='resnet18')
+# resnet152 2048
+# gen 3 vector time= 0.3303859233856201
+# insert + search redis 3 vector time= 0.011393070220947266
+# gen 3 vector time= 0.30896997451782227
+# insert + search redis 3 vector time= 0.014062166213989258
+# gen 3 vector time= 0.3175818920135498
+# insert + search redis 3 vector time= 0.007185220718383789
+
+# resnet18 512
+# gen 3 vector time= 0.10181188583374023
+# insert + search redis 3 vector time= 0.01618027687072754
+# gen 3 vector time= 0.07336091995239258
+# insert + search redis 3 vector time= 0.02003312110900879
+# gen 3 vector time= 0.09581732749938965
+# insert + search redis 3 vector time= 0.014796733856201172
+
+
+# efficientnet_b0 1280
+# gen 3 vector time= 0.6501989364624023
+# insert + search redis 3 vector time= 0.01497340202331543
+# gen 3 vector time= 0.6362099647521973
+# insert + search redis 3 vector time= 0.014306068420410156
+# gen 3 vector time= 0.6546528339385986
+# insert + search redis 3 vector time= 0.013219118118286133
+
+# efficientnet_b7 2560
+# gen 3 vector time= 4.143211841583252
+# insert + search redis 3 vector time= 0.013882875442504883
+# gen 3 vector time= 4.154558897018433
+# insert + search redis 3 vector time= 0.014590024948120117
+# gen 3 vector time= 4.071782112121582
+# insert + search redis 3 vector time= 0.013045072555541992
+
+IMAGE_VECTOR_DIMENSION = 2048
+img2vec = Img2Vec(cuda=False, model='resnet152')
 
 host = 'localhost'
 port = 10001
@@ -39,7 +74,7 @@ def get_image_feature_vector(file_path):
 
 PRODUCT_IMAGE_VECTOR_FIELD = 'image_image_vector'
 NUMBER_PRODUCTS = 1000
-IMAGE_VECTOR_DIMENSION = 512
+
 DISTANCE_METRIC = 'COSINE'
 
 def insert_redis(redis_conn, id, vector_bytes):
@@ -58,6 +93,8 @@ if __name__ == "__main__":
 
     create_flat_index(redis_conn, PRODUCT_IMAGE_VECTOR_FIELD,NUMBER_PRODUCTS,IMAGE_VECTOR_DIMENSION,DISTANCE_METRIC)
 
+    time_start = time.time()
+    
     vector = get_image_feature_vector('test01.jpg')
     # print(type(vector))
     # print(len(vector))
@@ -72,20 +109,20 @@ if __name__ == "__main__":
 
     vector_bytes = vector_float32.tobytes()
     # Encode 1
-    base64_bytes = base64.b64encode(vector_bytes)
-    base64_message = base64_bytes.decode()
+    # base64_bytes = base64.b64encode(vector_bytes)
+    # base64_message = base64_bytes.decode()
 
-    print(f'base64_message 1 {base64_message}')
+    # print(f'base64_message 1 {base64_message}')
 
     # Decode
-    base64_bytes = base64_message.encode()
-    message_bytes = base64.b64decode(base64_bytes)
+    # base64_bytes = base64_message.encode()
+    # message_bytes = base64.b64decode(base64_bytes)
 
     # Encode 2
-    base64_bytes = base64.b64encode(message_bytes)
-    base64_message = base64_bytes.decode()
+    # base64_bytes = base64.b64encode(message_bytes)
+    # base64_message = base64_bytes.decode()
 
-    print(f'base64_message 2 {base64_message}')
+    # print(f'base64_message 2 {base64_message}')
 
 
     vector_verify = get_image_feature_vector('test01_verify.jpg')
@@ -96,12 +133,17 @@ if __name__ == "__main__":
     vector_verify2_float32 = vector_verify2.astype(np.float32)
     vector_verify2_bytes = vector_verify2_float32.tobytes()
 
+    time_end = time.time()
+    print("gen 3 vector time= " + str(time_end - time_start))
+
     # item_metadata = {
     #     'id' : '001',
     #     'name' : 'Image 001',
     #     PRODUCT_IMAGE_VECTOR_FIELD: vector_bytes,
     # }
     # redis_conn.hset('001',mapping=item_metadata)
+    time_start = time.time()
+
     insert_redis(redis_conn, '001', vector_bytes)
     insert_redis(redis_conn, '002', vector_verify_bytes)
     insert_redis(redis_conn, '003', vector_verify2_bytes)
@@ -128,6 +170,8 @@ if __name__ == "__main__":
         print ('***************Product  found ************')
         print(product)
     
+    time_end = time.time()
+    print("insert + search redis 3 vector time= " + str(time_end - time_start))
 
 
 
