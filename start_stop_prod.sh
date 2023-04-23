a7d36a6bf380194805821d2ec39bed755992896e
duongdt <doanthanhduong11031990@gmail.com>
2023-04-23 18:04:59 +0700

    Update start_stop_prod.sh

--- start_stop_prod.sh
+++ start_stop_prod.sh
@@ -4,7 +4,10 @@ source venv/bin/activate
 # python -m pip install -r requirements.txt
 
 cd app
-gunicorn api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000  --daemon
+gunicorn api:app --access-logfile `pwd`/logs/gunicorn_access.log --error-logfile `pwd`/logs/gunicorn_error.log --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000  --daemon
 
 # Stop
 # kill -9 `ps aux | grep gunicorn | grep api:app | awk '{ print $2 }'`
+
+# docker run -d --name redis-stack-vector-books -v `pwd`/data-vector-redis/:/data -p 10001:6379 -p 13333:8001 redis/redis-stack:latest
+# redis UI port 13333
